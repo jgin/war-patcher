cd6f1942d99f7592c40bdf6ddf936f69864788e6
jgin <lvercelli.jasoftsolutions@gmail.com>
2013-10-16 21:03:31 -0500

    Inicio del proyecto

---
+++ war-patcher/src/main/java/org/jasoft/tools/war/patcher/App.java
@@ -0,0 +1,14 @@
+package org.jasoft.tools.war.patcher;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String filepath="D:\\dev\\prj\\sisprod2\\app\\src\\master\\sisprod-webapp\\target\\sisprod-webapp-0.7.0.war";
+        
+    }
+}
